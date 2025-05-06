6ecdc2098b3c017927c8cd68d206eca1570817ff
tearuiz <tearuiz871@gmail.com>
2025-05-06 16:55:57 -0700

    expose.html and expose.js update

--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -3,5 +3,68 @@
 window.addEventListener('DOMContentLoaded', init);
 
 function init() {
-  // TODO
+  // DOM elements
+  const hornSelect = document.getElementById('horn-select');
+  const volumeSlider = document.getElementById('volume-slider');
+  const volumeIcon = document.getElementById('volume-image');
+  const soundImage = document.getElementById('sound-image');
+  const audioElement = document.getElementById('horn-sound');
+  const playButton = document.getElementById('play-button');
+  
+  // initialize JSConfetti
+  const jsConfetti = new JSConfetti();
+
+  // horn selection 
+  hornSelect.addEventListener('change', () => {
+      const selectedHorn = hornSelect.value;
+      
+      // update images and audios
+      switch(selectedHorn) {
+          case 'air-horn':
+              soundImage.src = 'assets/images/air-horn.svg';
+              audioElement.src = 'assets/audio/air-horn.mp3';
+              break;
+          case 'car-horn':
+              soundImage.src = 'assets/images/car-horn.svg';
+              audioElement.src = 'assets/audio/car-horn.mp3';
+              break;
+          case 'party-horn':
+              soundImage.src = 'assets/images/party-horn.svg';
+              audioElement.src = 'assets/audio/party-horn.mp3';
+              break;
+          default:
+              soundImage.src = 'assets/images/no-image.png';
+              audioElement.src = '';
+      }
+  });
+
+  // volume slider 
+  volumeSlider.addEventListener('input', () => {
+      const volume = volumeSlider.value;
+      
+      const audioVolume = volume / 100;
+      audioElement.volume = audioVolume;
+      
+      // update volume level
+      if (volume == 0) {
+          volumeIcon.src = 'assets/icons/volume-level-0.svg';
+      } else if (volume < 33) {
+          volumeIcon.src = 'assets/icons/volume-level-1.svg';
+      } else if (volume < 67) {
+          volumeIcon.src = 'assets/icons/volume-level-2.svg';
+      } else {
+          volumeIcon.src = 'assets/icons/volume-level-3.svg';
+      }
+  });
+
+  // play button
+  playButton.addEventListener('click', () => {
+      audioElement.play();
+      
+      // party horn confetti case
+      if (hornSelect.value === 'party-horn' && jsConfetti) {
+          jsConfetti.addConfetti();
+      }
+  });
+
 }
