fc040b34271918d7e468c5bd870a83da70b1a0ab
tearuiz <tearuiz871@gmail.com>
2025-05-06 19:59:37 -0700

    update explore.js

--- assets/scripts/explore.js
+++ assets/scripts/explore.js
@@ -3,5 +3,51 @@
 window.addEventListener('DOMContentLoaded', init);
 
 function init() {
-  // TODO
+  //DOM elements
+  const synth = window.speechSynthesis;
+  const faceImg = document.querySelector('img');
+  const textArea = document.getElementById('text-to-speak');
+  const voiceSelect = document.getElementById('voice-select');
+  const speakButton = document.querySelector('button');
+  let voices = [];
+
+  //helper function for generating voices
+  function populateVoices() {
+    voices = synth.getVoices();
+
+    voiceSelect.innerHTML = '<option value="select" disabled selected>Select Voice:</option>';
+
+    voices.forEach(voice => {
+      const option = document.createElement('option');
+      option.textContent = `${voice.name} (${voice.lang})`;
+      option.value = voice.name;
+      voiceSelect.appendChild(option);
+    });
+  }
+
+  populateVoices();
+
+  // audio for voice 
+  speakButton.addEventListener('click', () => {
+    const text = textArea.value.trim();
+    if (!text || voiceSelect.value === 'select') return;
+
+    const utterance = new SpeechSynthesisUtterance(text);
+    const selectedVoice = voices.find(voice => voice.name === voiceSelect.value);
+    if (selectedVoice) {
+      utterance.voice = selectedVoice;
+    }
+
+    // update face image
+    utterance.onstart = () => {
+      faceImg.src = 'assets/images/smiling-open.png';
+    };
+
+    utterance.onend = () => {
+      faceImg.src = 'assets/images/smiling.png';
+    };
+
+
+    synth.speak(utterance);
+  });
 }
